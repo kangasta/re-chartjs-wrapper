d3d0ea69a94e63d5e665b6f927a06e0a63544672
Toni Kangas <toni.ar.kangas@gmail.com>
2020-03-16 23:16:38 +0200

    Add test for props precedence, fix bug in data handling

--- src/__tests__/chart.test.tsx
+++ src/__tests__/chart.test.tsx
@@ -2,11 +2,31 @@ import React from 'react';
 import { render } from '@testing-library/react';
 
 import { Chart } from '../index';
+import * as helpers from '../helpers';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import ChartJs from 'chart.js';
+import ChartJs, { ChartData, ChartOptions } from 'chart.js';
 jest.mock('chart.js');
 
 it('renders without crashing', (): void => {
-  render(<Chart configuration={{}}>Test</Chart>);
+  render(<Chart />);
+});
+
+it('defaults to configurations over other props', (): void => {
+  const dataSpy = jest.spyOn(helpers, 'getDataUpdater');
+  const optionsSpy = jest.spyOn(helpers, 'getOptionsUpdater');
+
+  const data: ChartData = { datasets: [{ data: [1] }] };
+  const options: ChartOptions = { responsive: true };
+
+  render(
+    <Chart
+      data={{ datasets: [] }}
+      options={{ responsive: false }}
+      configuration={{ data, options }}
+    />
+  );
+
+  expect(dataSpy).toHaveBeenCalledWith(data);
+  expect(optionsSpy).toHaveBeenCalledWith(options);
 });
